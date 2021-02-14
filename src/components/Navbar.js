93906bdfd1b9c4318db66e3c02e2006d31a46867
babylee2002 <babylee2002@gmail.com>
2021-02-13 19:55:02 -0500

    adding github page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,11 @@ export default function Navbar() {
    return (
       <div className='navbar'>
          <h3 className='App-header'>Lyric Finder</h3>
+         <div>
+            <a href='https://github.com/olee2002/lyrics-search-app'> 
+               <i class='fab fa-github'></i>
+            </a>
+         </div>
       </div>
    )
 }
